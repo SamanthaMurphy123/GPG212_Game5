46b23b71cd836eacfb7c7280ab1d87e9cc2a9939
MicroSillyWilly <1025946@student.sae.edu.au>
2023-03-16 23:34:14 +1100

    Added Extras

---
+++ Assets/Scripts/Jump/PlayerController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public Rigidbody2D rb;
+
+    private float moveX;
+
+    // For mobile controls
+   // public Joystick joystick;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get input from virtual joystick
+        // moveX = joystick.Horizontal * moveSpeed;
+        moveX = Input.GetAxis("Horizontal") * moveSpeed;
+        // Get input from keyboard or D-pad (only if virtual joystick isn't being used)
+        if (Mathf.Approximately(moveX, 0f))
+        {
+            moveX = Input.GetAxis("Horizontal") * moveSpeed;
+            
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 velocity = rb.velocity;
+        velocity.x = moveX;
+        rb.velocity = velocity;
+    }
+}
